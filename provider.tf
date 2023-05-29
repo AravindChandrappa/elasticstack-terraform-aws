1c3afd464e66ee5095c7769378be406db5fdddb7
AravindChandrappa <46191186+AravindChandrappa@users.noreply.github.com>
2023-05-29 17:18:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = ""
-  secret_key = ""
+  region     = "us-east-2"
+  access_key = "AKIAXOLJCO4BS3LJTT52"
+  secret_key = "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"
 }
